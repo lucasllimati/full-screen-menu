dade4fc1317ba322519fc56e0d7ddbe30a2faa23
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2022-08-07 15:08:40 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 
 function nav(){
   const navToggler = document.querySelector(".js-nav-toggler");
+  const nav = document.querySelector(".js-nav");
 
   const togglerNav = () => {
     navToggler.classList.toggle("active");
+    nav.classList.toggle("open");
   }
 
   navToggler.addEventListener("click", togglerNav);
